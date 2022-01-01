4622ee816da9134885b89610bd63b6787a9a3332
bherendeen <her19051@byui.edu>
2022-01-01 13:50:59 -0700

    commit Jan 1

--- js/main.js
+++ js/main.js
@@ -17,6 +17,6 @@
     document.getElementById("contDisp").innerHTML = parentTag;
     
     function myFunction(tableCont) {
-        parentTag += `<li><a href="${tableCont.url}" target="_blank" rel="noopener noreferrer">${tableCont.label}</a> ${tableCont.label}</li>`;
+        parentTag += `<li><a href="${tableCont.url}" target="_blank" rel="noopener noreferrer">${tableCont.label}</a></li>`;
 }
 }());
